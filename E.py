03fbfe3b84242c36011865c996b8a299b2fc2e28
xDynamo01 <arthurahlx@gmail.com>
2025-09-06 18:00:28 -0300

    Softmax activation

--- E.py
+++ E.py
@@ -1,16 +1,12 @@
-import math
 import numpy as np
+import nnfs
+nnfs.init()
 
-
-layer_outputs = [4.8, 1.21, 2.385]
-
-E = math.e
+layer_outputs = [[4.8, 1.21, 2.385],
+                 [8.9, -1.81, 0.2],
+                 [1.41, 1.051, 0.026]]
 
 exp_values = np.exp(layer_outputs)
-
-print(exp_values)
-
-norm_values = exp_values / np.sum(exp_values)
+norm_values = exp_values / (np.sum(layer_outputs, axis=1, keepdims=True))
 
 print(norm_values)
-print(sum(norm_values))
