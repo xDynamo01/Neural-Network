0c38f773050d05d62ae4b2e473c6e3eedb6020ba
xDynamo01 <arthurahlx@gmail.com>
2025-09-08 16:02:35 -0300

    log

--- log.py
+++ log.py
@@ -4,7 +4,21 @@ e ** x = b
 import numpy as np
 import math
 
-b = 5.2
+#b = 5.2
 
-print(np.log(b))  # natural log, base e
-print(math.e ** 1.6486586255873816)
+#print(np.log(b))  # natural log, base e
+#print(math.e ** 1.6486586255873816)
+
+softmax_outputs = [0.7, 0.1, 0.2]
+targer_outputs = [1, 0, 0]
+
+loss = -(math.log(softmax_outputs[0])*targer_outputs[0] +
+         math.log(softmax_outputs[1])*targer_outputs[1] +
+         math.log(softmax_outputs[2])*targer_outputs[2])
+
+print(loss)
+loss = -math.log(softmax_outputs[0])
+print(loss)
+
+print(math.log(0.7))
+print(-math.log(0.5))
